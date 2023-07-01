f06e1ad2c52b0c760f134fdddb72452b0c6cd2aa
Asutosh  Chamappaji Urs <48209722+ASVT0SH@users.noreply.github.com>
2023-07-01 10:21:50 +0530

    implemented needs_service() for super class

--- battery/nubbin_battery.py
+++ battery/nubbin_battery.py
@@ -1,4 +1,5 @@
 from battery import Battery
+from datetime import datetime
 
 class NubbinBattery (Battery):
     def __init__(self, last_service_date, current_date):
@@ -7,4 +8,8 @@ class NubbinBattery (Battery):
         self.last_service_date = last_service_date
 
     def needs_service(self)->bool:
-        return self.current_date > self.last_service_date 
+        service_threshold_date = self.last_service_date.replace(year=self.last_service_date.year + 4)
+        if service_threshold_date < datetime.today().date() or self.engine_should_be_serviced():
+            return True
+        else:
+            return False 
--- battery/spindler_battery.py
+++ battery/spindler_battery.py
@@ -1,5 +1,5 @@
 from battery import Battery
-
+from datetime import datetime
 class SpindlerBattery (Battery):
     def __init__(self, last_service_date, current_date):
         super().__init__(last_service_date)
@@ -7,4 +7,9 @@ class SpindlerBattery (Battery):
         self.last_service_date = last_service_date
         
     def needs_service(self)->bool:
-        return self.current_date > self.last_service_date 
+        service_threshold_date = self.last_service_date.replace(year=self.last_service_date.year + 2)
+        if service_threshold_date < datetime.today().date() or self.engine_should_be_serviced():
+            return True
+        else:
+            return False
+
