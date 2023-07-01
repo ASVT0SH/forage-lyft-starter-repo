c536081eef798e5ff8ace982655e25588708247b
Asutosh  Chamappaji Urs <48209722+ASVT0SH@users.noreply.github.com>
2023-07-01 10:11:47 +0530

    basic refactor, created engine superclass and battery superclass

---
+++ battery/battery.py
@@ -0,0 +1,5 @@
+class Battery:
+    """Battery class, which is imported by
+    Spindler and Nubbin"""
+    def needs_service(self)->bool:
+        pass
---
+++ engine/engine.py
@@ -0,0 +1,5 @@
+class Engine:
+    """Class engine, super class of
+    Capulet, Willoughby and Sternman"""
+    def needs_service(self) -> bool:
+        pass
